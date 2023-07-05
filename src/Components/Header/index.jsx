99908535a5b25a8cc82dd969a3861542703434de
IdrisVeli1987 <idrisveliev@gmail.com>
2023-07-05 10:57:30 +0400

    add top Header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,14 +1,22 @@
 import { Box, Container, Stack, Typography } from "@mui/material";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 const Header = () => {
   return (
     <>
-      <Box sx={{ backgroundColor: "rgba(255, 192, 31, 1)" }}>
+      <Box sx={{ backgroundColor: "rgba(255, 192, 31, 1)", py: 2 }}>
         <Container>
           <Stack flexDirection="row" justifyContent="space-between">
             <Typography>
               +994(12) 123 45 67 | Fətəli Xan Xoyski 111A, Bakı Azərbaycan
             </Typography>
+            <Stack flexDirection="row" gap={2}>
+              <InstagramIcon />
+              <FacebookIcon />
+              <TwitterIcon />
+            </Stack>
           </Stack>
         </Container>
       </Box>
